c3346d75d8e6ab22fc3b20ab58c02cf0e6d50466
royalfig <ryan.feigenbaum@gmail.com>
2019-11-25 13:41:46 -0500

    remove node modules

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean-css');
+
+gulp.task('css', () => {
+    gulp.src
+})
